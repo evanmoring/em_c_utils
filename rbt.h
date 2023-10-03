7050cfe2f9bf37846760c6ff14f369c8cc824906
evan <epmoring@gmail.com>
2023-10-03 00:25:17 -0700

    rbt insert working

--- rbt.h
+++ rbt.h
@@ -1,145 +1,127 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <limits.h>
 
 enum Color { RED = 0, BLACK = 1};
+enum Children { LEFT = 0, RIGHT = 1};
 
 typedef struct rbt_node {
     struct rbt_node* parent;
-    struct rbt_node* left;
-    struct rbt_node* right;
+    struct rbt_node* c[2]; // children, LEFT [0] RIGHT [1]
     int value;
     int color;
 } rbt_node;
 
 typedef struct rbt {
     struct rbt_node* root;
-    struct rbt_node* null;
+    struct rbt_node* nill;
 } rbt;
 
 
-rbt_node* init_rbt (int value){
-    rbt_node * header = malloc(sizeof (rbt_node));
-    header->left = NULL;
-    header->right = NULL;
-    header->parent = NULL;
-    header->value = value;
-    header->color = BLACK;
-    return header;
+rbt* init_rbt (){
+    rbt * tree = malloc(sizeof(rbt));
+    tree->nill = malloc(sizeof (rbt_node));
+    tree->nill->c[LEFT] = tree->nill;
+    tree->nill->c[RIGHT] = tree->nill;
+    tree->nill->parent = tree->nill;
+    tree->nill->value = INT_MIN;
+    tree->nill->color = BLACK;
+    tree->root = tree->nill;
+    return tree;
 }
 
-void left_rotate(rbt_node* n){
+void rotate(rbt* tree, rbt_node* n, int d){
+    // d is RIGHT or LEFT
     // TODO test
-    rbt_node* y = n->right;
-    n->right = y->left;
-    if (y->left != NULL){
-        y->left->parent = n;
+    rbt_node* y = n->c[!d];
+    n->c[!d] = y->c[d];
+    if (y->c[d] != tree->nill){
+        y->c[d]->parent = n;
     }
     y->parent = n->parent;
-    if (n->parent == NULL){
-        y->parent = NULL;
+    if (n->parent == tree->nill){
+        tree->root = y;
     } 
-    else if (n == n->parent->left){
-        n->parent->left = y;
+    else if (n == n->parent->c[d]){
+        n->parent->c[d] = y;
     }
     else {
-        n->parent->right = y;
+        n->parent->c[!d] = y;
     }
-    y->left = n;
-    n->parent = y;
-}
-
-void right_rotate(rbt_node* n){
-    // TODO test
-    rbt_node* y = n->left;
-    n->right = y->right;
-    if (y->right != NULL){
-        y->right->parent = n;
-    }
-    y->parent = n->parent;
-    if (n->parent == NULL){
-        y->parent = NULL;
-    } 
-    else if (n == n->parent->right){
-        n->parent->right = y;
-    }
-    else {
-        n->parent->left = y;
-    }
-    y->right = n;
+    y->c[d] = n;
     n->parent = y;
 }
 
 void walk_rbt (rbt_node* header){
     if (header != NULL){
-        walk_rbt(header->left);
+        walk_rbt(header->c[LEFT]);
         printf("%d\n",header->value);
-        walk_rbt(header->right);
+        walk_rbt(header->c[RIGHT]);
     }
 }
 
-void insert_fixup_rbt(rbt_node* header, rbt_node* node){
+void insert_fixup_rbt(rbt* tree, rbt_node* node){
+    int d;
     while (node->parent->color == RED){
-        if (node->parent == node->parent->parent->left){
-            rbt_node* y = node->parent->parent->right; // uncle
-            if (y->color == RED){
-                // case 1, uncle is red
-                node->parent->color = BLACK;
-                y->color = BLACK;
-                node->parent->parent->color = RED;
-                node = node->parent->parent;
-            }
-            else if (node == node->parent->right){
+        if (node->parent == node->parent->parent->c[LEFT]){
+            d = LEFT;
+        }
+        else {d = RIGHT;}
+        rbt_node* y = node->parent->parent->c[!d]; // uncle
+        if (y->color == RED){
+            // case 1, uncle is red
+            node->parent->color = BLACK;
+            y->color = BLACK;
+            node->parent->parent->color = RED;
+            node = node->parent->parent;
+        }
+        else {
+            if (node == node->parent->c[!d]){
                 // case 2 node is a right child
                 node = node->parent;
-                left_rotate(node);
+                rotate(tree, node, d);
             }
-            else {
-                // case 3 node is a left child
-                node->parent->color = BLACK;
-                node->parent->parent = RED;
-                right_rotate(node->parent->parent);
-            }
-        }
-        else{
-            // TODO same for reverse
+            // case 3 node is a left child
+            node->parent->color = BLACK;
+            node->parent->parent->color = RED;
+            rotate(tree, node->parent->parent, !d);
         }
     }
+    tree->root->color = BLACK;
 }
 
-
-void insert_rbt (rbt_node* header, int value){
+void insert_rbt (rbt* tree, int value){
     //TODO test
-    rbt_node * p_new_node = malloc(sizeof (rbt_node));
+    rbt_node * header = tree->root;
+    rbt_node * p_new_node = malloc(sizeof(rbt_node));
     p_new_node->value = value;
-    p_new_node->left = NULL;
-    p_new_node->right = NULL;
+    p_new_node->c[LEFT] = tree->nill;
+    p_new_node->c[RIGHT] = tree->nill;
     p_new_node->color = RED;
-    p_new_node->parent = NULL;
+    p_new_node->parent = tree->nill;
+
     rbt_node* x = header;
-    rbt_node* y = NULL;
-    while (x != NULL){
+    rbt_node* y = tree->nill;
+    while (x != tree->nill){
+        y = x;
         if (value > x->value){
-            y = x;
-            x = x->right;
+            x = x->c[RIGHT];
         }
         else {
-            y = x;
-            x = x->left;
+            x = x->c[LEFT];
         }
     }
-    if(y == NULL){ // handle if tree is empty
-        header = p_new_node;
+    p_new_node->parent = y;
+    if(y == tree->nill){ // handle if tree is empty
+        tree->root = p_new_node;
+    }
+    else if (value > y->value){
+         y->c[RIGHT] = p_new_node;
     }
     else {
-        if (value > y->value){
-            y->right = p_new_node;
-        }
-        else {
-            y->left = p_new_node;
-        }
-        p_new_node->parent = y;
+        y->c[LEFT] = p_new_node;
     }
-    insert_fixup_rbt(header,p_new_node);
+    insert_fixup_rbt(tree,p_new_node);
 }
 
 rbt_node* search_rbt (rbt_node* header, int val){
@@ -147,23 +129,23 @@ rbt_node* search_rbt (rbt_node* header, int val){
         return header;
     }
     if (val < header->value){
-        return search_rbt(header->left, val);
+        return search_rbt(header->c[LEFT], val);
     }
     else {
-        return search_rbt(header->right, val);
+        return search_rbt(header->c[RIGHT], val);
     }
 }
 
 rbt_node* min_rbt (rbt_node* header){
-    while (header->left != NULL){
-        header = header->left;
+    while (header->c[LEFT] != NULL){
+        header = header->c[LEFT];
     }
     return header;
 }
 
 rbt_node* max_rbt (rbt_node* header){
-    while (header->right != NULL){
-        header = header->right;
+    while (header->c[RIGHT] != NULL){
+        header = header->c[RIGHT];
     }
     return header;
 }
@@ -173,11 +155,11 @@ int transplant_rbt (rbt_node * replacee, rbt_node* replacer){
     if(replacee->parent == NULL){
         replacer->parent = NULL;
     }
-    else if (replacee == replacee->parent->left){
-        replacee->parent->left = replacer;
+    else if (replacee == replacee->parent->c[LEFT]){
+        replacee->parent->c[LEFT] = replacer;
     }
     else {
-        replacee->parent->right = replacer;
+        replacee->parent->c[RIGHT] = replacer;
     }
     if (replacer != NULL){
         replacer->parent = replacee->parent;
@@ -187,24 +169,24 @@ int transplant_rbt (rbt_node * replacee, rbt_node* replacer){
 rbt_node* delete_rbt (rbt_node* header, rbt_node* to_delete){
     // three cases: 
     // no left child, we replace with right child
-    if (to_delete->left == NULL){
-        transplant_rbt(to_delete, to_delete->right);
+    if (to_delete->c[LEFT] == NULL){
+        transplant_rbt(to_delete, to_delete->c[RIGHT]);
     }
     // no right child, we replace with left child
-    else if (to_delete->right == NULL){
-        transplant_rbt(to_delete, to_delete->left);
+    else if (to_delete->c[RIGHT] == NULL){
+        transplant_rbt(to_delete, to_delete->c[LEFT]);
     }
     // take the minimum of the subtree rooted by the right child and replace to_delete with it
     else {
-        rbt_node* replacer = min_rbt(to_delete->right);
+        rbt_node* replacer = min_rbt(to_delete->c[RIGHT]);
         if (replacer->parent != to_delete){
-            transplant_rbt(replacer, replacer->right);
-            replacer->right = to_delete->right;
-            replacer->right->parent = replacer;
+            transplant_rbt(replacer, replacer->c[RIGHT]);
+            replacer->c[RIGHT] = to_delete->c[RIGHT];
+            replacer->c[RIGHT]->parent = replacer;
         }
         transplant_rbt(to_delete, replacer);
-        replacer->left = to_delete->left;
-        replacer->left->parent = replacer;
+        replacer->c[LEFT] = to_delete->c[LEFT];
+        replacer->c[LEFT]->parent = replacer;
         if(replacer->parent == NULL){
             header = replacer;     
         }
@@ -215,11 +197,11 @@ rbt_node* delete_rbt (rbt_node* header, rbt_node* to_delete){
 }
 
 rbt_node* successor_rbt(rbt_node* start){
-    if(start->right != NULL){
-        return min_rbt(start->right);
+    if(start->c[RIGHT] != NULL){
+        return min_rbt(start->c[RIGHT]);
     }
     rbt_node* par = start->parent;
-    while (par != NULL && start == par->right){
+    while (par != NULL && start == par->c[RIGHT]){
         start = par;
         par = par->parent;
     }
@@ -227,31 +209,31 @@ rbt_node* successor_rbt(rbt_node* start){
 }
 
 rbt_node* predecessor_rbt(rbt_node* start){
-    if(start->left != NULL){
-        return max_rbt(start->left);
+    if(start->c[LEFT] != NULL){
+        return max_rbt(start->c[LEFT]);
     }
     rbt_node* par = start->parent;
-    while (par != NULL && start == par->left){
+    while (par != NULL && start == par->c[LEFT]){
         start = par;
         par = par->parent;
     }
     return par;
 }
 
-void print_tree(rbt_node * header, int depth){
+void print_tree(rbt* tree, rbt_node * header, int depth){
     printf("%d",header->value);
-    if(header->left != NULL){
+    if(header->c[LEFT] != tree->nill){
         printf("  ");
-        print_tree(header->left, depth + 1);
+        print_tree(tree, header->c[LEFT], depth + 1);
     }
-    if (header->right != NULL){
+    if (header->c[RIGHT] != tree->nill){
         printf("\n");
         for (int i = 0; i<=depth; i++){
             printf("   ");
         }
-        print_tree(header->right, depth + 1);
+        print_tree(tree, header->c[RIGHT], depth + 1);
     }
-    if(header->parent == NULL){
+    if(header->parent == tree->nill){
         printf("\n");
     }
 }
